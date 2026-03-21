a10f4c769dada0c2611daaaa0dac0419d9cdc3b5
rangabharathkumar <162026811+rangabharathkumar@users.noreply.github.com>
2026-03-21 12:47:08 +0530

    Time: 1 ms (58.37%) | Memory: 47.6 MB (34.69%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -4,13 +4,10 @@ class Solution {
         int bottom=x+k-1;
         int i=y;
         while(top <(x+k/2)){
-            System.out.println(".");
             while(i<=y+k-1){
             grid[top][i]=grid[top][i]+grid[bottom][i];
             grid[bottom][i]=grid[top][i]-grid[bottom][i];
             grid[top][i]=grid[top][i]-grid[bottom][i];
-            System.out.println(grid[top][i]+"1");
-            System.out.println(grid[bottom][i]);
             i++;
         }
         i=y;
