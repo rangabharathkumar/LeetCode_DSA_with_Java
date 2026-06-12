be0e5470b157a253a8afed65e6d3009a3f855949
rangabharathkumar <162026811+rangabharathkumar@users.noreply.github.com>
2026-06-12 07:22:51 +0530

    Time: 708 ms (36.46%) | Memory: 0B (100.00%) - LeetSync

--- SQL/2087-confirmation-rate/confirmation-rate.sql
+++ SQL/2087-confirmation-rate/confirmation-rate.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-SELECT Signups.user_id,ROUND(IFNULL (COUNT(CASE WHEN Confirmations.action="confirmed" THEN 1 ELSE NULL END)/COUNT(Confirmations.user_id),0),2) AS confirmation_rate FROM Signups LEFT JOIN Confirmations ON Signups.user_id = Confirmations.user_id GROUP BY Signups.user_id;
+SELECT Signups.user_id,
+ROUND(IFNULL (COUNT(CASE WHEN Confirmations.action="confirmed" THEN 1 ELSE NULL END)/COUNT(Confirmations.user_id),0),2) 
+AS confirmation_rate 
+FROM Signups 
+LEFT JOIN Confirmations 
+ON Signups.user_id = Confirmations.user_id GROUP BY Signups.user_id;
